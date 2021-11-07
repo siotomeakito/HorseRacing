217417350767edccf5813e59d0e8edef9390568e
DanielRETsai <DanielRETsai@mobiledrivetech.com>
2021-11-07 22:53:03 +0800

    1107 home

---
+++ app/src/main/java/com/example/horseracing/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.horseracing
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/example/horseracing/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.horseracing
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
